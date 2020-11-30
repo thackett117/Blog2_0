73790a052195febcecc11843da84d1de79d25139
thackett117 <thackett117@gmail.com>
2020-11-30 13:26:56 -0600

    DB_PASSWORD

--- src/server/config/production.ts
+++ src/server/config/production.ts
@@ -3,7 +3,7 @@ export default {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         port: process.env.DB_PORT,
-        password: process.env.DB_PASS,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_SCHEMA
     },
     auth: {
